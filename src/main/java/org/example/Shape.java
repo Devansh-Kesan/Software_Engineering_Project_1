f334a8abce49623069aca2d9affcff7f3a3a0734
Devansh Kesan <devanshkesan101@gmail.com>
2025-08-27 15:46:52 +0530

    Added Shape interface

---
+++ src/main/java/org/example/Shape.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Shape interface shows abstraction.
+ * It only tells that shape can be drawn,
+ * but does not provide any information about how it will be drawn.
+ */
+public interface Shape {
+
+    /**
+     * method to draw shape.
+     */
+    void draw();
+}
